f63ea503a7d1c3f70dd213dae90397d5fc3877f7
kamem <develo0@gmail.com>
2020-10-23 10:57:00 +0900

    scroll-parallax-effectの追加

--- src/main.js
+++ src/main.js
@@ -2,5 +2,6 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import Parallax from 'scroll-parallax-effect/vue'
 
-createApp(App).use(store).use(router).mount('#app')
+createApp(App).use(store).use(router).use(Parallax).mount('#app')
